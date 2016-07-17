831169e0485cf1c8457b52f2093fd1f1bfcd43e1
Mitchell Hudson <mitchellhudson@mitchells-mbp.gateway.sonic.net>
2016-07-16 20:12:14 -0700

    update
    Added camera, camera follows ball.

--- Bezier Landscape/GameScene.swift
+++ Bezier Landscape/GameScene.swift
@@ -20,16 +20,26 @@ class GameScene: SKScene {
     var centerY: CGFloat!
     var stepX: CGFloat!
     var stepXhalf: CGFloat!
-    var steps: Int = 10
+    
+    // Set the width of a landscape section
+    var sectionWidth: CGFloat = 10000
+    // Set the number of curves drawn into each section
+    var steps: Int = 80
+    // Set the amount of
+    var slopeRange: UInt32 = 140
+    var slopeMinChange: CGFloat = 20
+    var slopeRangeCenter: CGFloat = 40
+    
     var lastLandscapeY: CGFloat = 0
     
     var lastUpdateTimeInterval: CFTimeInterval = 0
     
+    var cameraNode: SKCameraNode!
     
     func scrollLandscapes(deltaTime: CFTimeInterval) {
         for landscape in landscapes {
             // Use
-            landscape.position.x -= 20 * CGFloat(deltaTime)
+            landscape.position.x -= 40 * CGFloat(deltaTime)
             if landscape.position.x < leftLimit {
                 // Recycle this landscape and draw a new contour
                 landscape.position.x += landscapeRight
@@ -56,7 +66,8 @@ class GameScene: SKScene {
             let c1 = CGPoint(x: x + stepXhalf, y: y)
             // Set the x and y for the new point
             x = CGFloat(i) * stepX
-            y = CGFloat(arc4random() % 100) - 100 + centerY
+            // y = CGFloat(arc4random() % 100) - 100 + centerY
+            y -= (CGFloat(arc4random() % slopeRange) + slopeMinChange) - slopeRangeCenter
             let p = CGPoint(x: x, y: y)
             // Set control point 2
             let c2 = CGPoint(x: x - stepXhalf, y: y)
@@ -83,23 +94,42 @@ class GameScene: SKScene {
 
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        cameraNode = SKCameraNode()
+        addChild(cameraNode)
+        camera = cameraNode
+        
         let ballSize = CGSize(width: 30, height: 30)
         ball = SKShapeNode(ellipseOfSize: ballSize)
         ball.fillColor = UIColor(white: 1, alpha: 0.3)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: 15)
+        ball.physicsBody?.linearDamping = 0.8
         ball.position.x = view.frame.width / 2
         ball.position.y = view.frame.height - 100
         addChild(ball)
+        
+        /*
+        let b = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 10, height: 10))
+        b.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 10, height: 10))
+        b.physicsBody?.dynamic = false
+        addChild(b)
+        
+        let anchor = CGPoint(x: view.frame.width / 2, y: 50)
+        b.position = anchor
+        let vector = CGVector(dx: 0, dy: view.frame.height - 100)
+        let joint = SKPhysicsJointSliding.jointWithBodyA(ball.physicsBody!, bodyB: b.physicsBody!, anchor: anchor, axis: vector)
+        physicsWorld.addJoint(joint)
+        */
         
         // Get some numbers we need to draw stuff
         centerY = view.frame.height / 2
         lastLandscapeY = centerY
         
-        stepX = view.frame.width / CGFloat(steps)
+        stepX = sectionWidth / CGFloat(steps)
         stepXhalf = stepX / 2
         
-        landscapeWidth = view.frame.width
-        leftLimit = 0 - view.frame.width / 1
+        landscapeWidth = sectionWidth
+        leftLimit = -landscapeWidth
         
         // Make two "landscapes" these are SKShapeNodes that will
         for i in 0 ..< 2 {
@@ -131,13 +161,17 @@ class GameScene: SKScene {
             timeSinceLast = 1.0 / 60.0
             lastUpdateTimeInterval = currentTime
         }
-        scrollLandscapes(timeSinceLast)
+        // scrollLandscapes(timeSinceLast)
         
+        /*
         if ballRolling {
             ball.physicsBody?.applyTorque(-2000)
             ball.physicsBody?.applyForce(CGVector(dx: 50, dy: 0))
             // print(ball.physicsBody?.angularVelocity)
         }
+        */
+        
+        cameraNode.position = ball.position
         
     }
 }
